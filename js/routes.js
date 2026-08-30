7153f98f968ff207773c9f0fd15f93f870e67a44
1FireXY1 <50220934+1FireXY1@users.noreply.github.com>
2026-08-30 00:50:55 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import LegacyList from './pages/LegacyList.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/legacylist', component: LegacyList },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
